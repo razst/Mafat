d14487717a9f9d16426d6519f3b5ff6e3cee3baf
Raz <raz_st@hotmail.com>
2020-07-16 20:37:45 +0300

    concate noise sounds

--- ReduceNoise.py
+++ ReduceNoise.py
@@ -2,33 +2,36 @@ import noisereduce as nr
 import librosa
 import sounddevice as sd
 from scipy.io.wavfile import write
+import numpy as np
 
 baseDir = "c:/dev/Mafat"
 
 
-def redouceNoise(soundData, noiseFile):
-    # load noise only file
-    print("loading noise file...")
-    dataNoise, rate = librosa.load(baseDir+"/sample_files/ActualSounds/n1.wav")
-    # perform noise reduction
-    print("reducing noise from sound...")
-    reduced_noise = nr.reduce_noise(audio_clip=data, noise_clip=dataNoise, prop_decrease=1.0, verbose=False)
-
-
 
 def reduceSourceFile(sourceFile):    
+    print("loading noise file...")
+    dataNoise1, rate = librosa.load(baseDir+"/sample_files/ActualSounds/n1.wav")
+    dataNoise2, rate = librosa.load(baseDir+"/sample_files/ActualSounds/n2.wav")
+    dataNoise3, rate = librosa.load(baseDir+"/sample_files/ActualSounds/n3.wav")
+    dataNoise4, rate = librosa.load(baseDir+"/sample_files/ActualSounds/n4.wav")
+    dataNoiseAll = np.concatenate((dataNoise1,dataNoise2,dataNoise3,dataNoise4))
+     
+
+
     # load sound file (sound + noise) data + rate 
     #rate, data = wavfile.read("c:/temp/TEST.wav")
     print("loading sound file...")
     data, rate = librosa.load(sourceFile)
-    redouceNoise(data,"")
+
+    print("reducing noise from sound...")
+    noise_reduce = nr.reduce_noise(audio_clip=data, noise_clip=dataNoiseAll, prop_decrease=1.0, verbose=False)
 
     print("Writing output file...")
     # write output sound without noise
-    write(baseDir+'/sample_files/ActualSounds/reduced4_twice_0.75.wav', rate, reduced_noise)  # Save as WAV file 
+    write(baseDir+'/sample_files/ActualSounds/reduced.wav', rate, noise_reduce)  # Save as WAV file 
 
 
-reduceSourceFile(baseDir+"/sample_files/ActualSounds/s4.wav")
+reduceSourceFile(baseDir+"/sample_files/ActualSounds/s3.wav")
 # print("playing output file...")
 # # play the WAV file 
 # sd.play(reduced_noise, rate)
